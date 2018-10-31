a8311f5e3e406b737c02946932e2db5b166152d5
lucasdlg5 <lucasdomingosleao55@hotmail.com>
2018-10-31 20:41:42 -0300

    Adicionados telas aplicativo

---
+++ AndroidBankev/app/src/main/java/sjc/fatec/sp/br/bankev20/activity_CreditCardBill.java
@@ -0,0 +1,13 @@
+package sjc.fatec.sp.br.bankev20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_CreditCardBill extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__credit_card_bill);
+    }
+}
---
+++ AndroidBankev/app/src/main/java/sjc/fatec/sp/br/bankev20/activity_contas_a_pagar.java
@@ -0,0 +1,13 @@
+package sjc.fatec.sp.br.bankev20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_contas_a_pagar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contas_a_pagar);
+    }
+}
