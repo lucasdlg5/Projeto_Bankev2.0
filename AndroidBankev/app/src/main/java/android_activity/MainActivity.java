1984535c329744d6f1baf808701a2c41c9254cbf
LUCAS DOMINGOS LEAO GOMES <lucas.gomes31@fatec.sp.gov.br>
2018-11-30 16:48:48 -0200

    Tela Novo Cadastro e Mensagens de Alerta adicionados adequadamente

--- AndroidBankev/app/src/main/java/android_activity/MainActivity.java
+++ AndroidBankev/app/src/main/java/android_activity/MainActivity.java
@@ -1,14 +1,15 @@
 package android_activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.NoSuchElementException;
-
-import hello.CadastroUsuarioActivity;
 import hello.Conexao;
 import sjc.fatec.sp.br.bankev20.R;
 
@@ -23,42 +24,60 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    private void toastShowMsg(String textMsg){
+
+        Context context = getApplicationContext();
+        CharSequence textToast = textMsg;
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, textToast, duration);
+        toast.show();
+    }
     public void onClickSearch(View view){
+
+
+
+
         TextView txtLogin = findViewById(R.id.txtLogin);
         TextView txtSenha = findViewById(R.id.txtSenha);
         TextView lblResultado = findViewById(R.id.lblResultado);
 
-        //String login = String.valueOf(txtLogin.getText());
-        //String senha = String.valueOf(txtSenha.getText());
+        String login = String.valueOf(txtLogin.getText());
+        String senha = String.valueOf(txtSenha.getText());
 
-        String login = "lucasdlg";
-        String senha = "123";
+//        String login = "lucasdlg";
+//        String senha = "123";
         try{
         Conexao cnn = new Conexao();
         boolean doLogin = cnn.sendPostLogin(login, senha);
         //List<Usuario> teste = cnn.sendGetRecuperaUsuario(login);
         if(doLogin){
+
             //lblResultado.setText(String.valueOf(teste.size()));
-            lblResultado.setText("Seja bem vindo!");
+//            lblResultado.setText("Seja bem vindo!");
+            toastShowMsg("Seja bem vindo!!");
+//
             nextWindow = new Intent(view.getContext(), activity_home.class);
             startActivity(nextWindow);
 
         }else
         {
             //nao entrou, aparecer mensagem
-            lblResultado.setText("Vefique a Ortografia, ou tente novamente, a aplicação está alocada no Heroku, isso pode causar incosistências.\n\nDados de tentativa de login:\nUsername: "+login+"\nSenha: "+senha);
+//            lblResultado.setText("Vefique a Ortografia, ou tente novamente, a aplicação está alocada no Heroku, isso pode causar incosistências.\n\nDados de tentativa de login:\nUsername: "+login+"\nSenha: "+senha);
+            toastShowMsg("Vefique a Ortografia, ou tente novamente, a aplicação está alocada no Heroku, isso pode causar incosistências.\n\nDados de tentativa de login:\nUsername: "+login+"\nSenha: "+senha);
 
         }
         }catch(Exception e){
-            lblResultado.setText(e.toString());
+//            lblResultado.setText(e.toString());
+            toastShowMsg(e.toString());
         }
 
     }
 
-    public void openCadastroBanco(View v){
-        Intent intent = new Intent(v.getContext(), CadastroUsuarioActivity.class);
-        startActivity(intent);
-    }
+//    public void openCadastroBanco(View v){
+//        Intent intent = new Intent(v.getContext(), CadastroUsuarioActivity.class);
+//        startActivity(intent);
+//    }
 
     public void openRegisterPage(View v){
         nextWindow = new Intent(v.getContext(), activity_signup.class);
--- AndroidBankev/app/src/main/java/android_activity/activity_signup.java
+++ AndroidBankev/app/src/main/java/android_activity/activity_signup.java
@@ -1,15 +1,34 @@
 package android_activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import hello.Conexao;
 import sjc.fatec.sp.br.bankev20.R;
 
 public class activity_signup extends AppCompatActivity {
 
     Intent nextWindow;
+
+
+    private void toastShowMsg(String textMsg){
+
+        Context context = getApplicationContext();
+        CharSequence textToast = textMsg;
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, textToast, duration);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,46 @@ public class activity_signup extends AppCompatActivity {
     public void openLoginPage(View v){
         nextWindow = new Intent(v.getContext(), MainActivity.class);
         startActivity(nextWindow);
+    }
+
+    public void onClickSearch(View view){
+        TextView txtNomeCompleto = findViewById(R.id.txtNomeCompleto);
+        TextView txtCPF = findViewById(R.id.txtCPF);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+        TextView txtLogin = findViewById(R.id.txtLogin);
+        TextView txtSenha = findViewById(R.id.txtSenha);
+        TextView txtConta = findViewById(R.id.txtConta);
+        TextView lblCadResultado = findViewById(R.id.lblCadResultado);
+
+        String nome = String.valueOf(txtNomeCompleto.getText());
+        String cpf = String.valueOf(txtCPF.getText());
+        String email = String.valueOf(txtEmail.getText());
+        String login = String.valueOf(txtLogin.getText());
+        String senha = String.valueOf(txtSenha.getText());
+        String conta = String.valueOf(txtConta.getText());
+
+
+        Conexao cnn = new Conexao();
+        try {
+            if(cnn.SendGetCadUsuario(nome, cpf, email, login, senha, conta)){
+                toastShowMsg("Usuario Cadastrado com Sucesso");
+                nextWindow = new Intent(view.getContext(), MainActivity.class);
+                startActivity(nextWindow);
+//                lblCadResultado.setText("Usuario Cadastrado com Sucesso");
+
+            }
+            else{
+//                lblCadResultado.setText("Falha ao cadastrar, tente novamente em breve");
+                toastShowMsg("Falha ao cadastrar, tente novamente em breve");
+
+            }
+
+        } catch (IOException e) {
+            lblCadResultado.setText(e.getMessage());
+            toastShowMsg(e.getMessage());
+        } catch (JSONException e) {
+            lblCadResultado.setText(e.getMessage());
+            toastShowMsg(e.getMessage());
+        }
     }
 }
